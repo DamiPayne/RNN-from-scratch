cfca42b69d475d353786abc28e1e91d4a15e9b55
Damilola Payne <DamiPayne@users.noreply.github.com>
2017-06-17 11:28:36 +0100

    Update RNN.py

--- RNN.py
+++ RNN.py
@@ -92,7 +92,7 @@ for j in range(10000):
     c_int = a_int + b_int
     c = interger_to_binary[c_int]
 
-    #where we will store the binary encoded guess
+    # Where we will store the binary encoded guess
     d = np.zeros_like(c)
 
     overallError = 0
@@ -101,31 +101,31 @@ for j in range(10000):
     layer_1_values = list()
     layer_1_values.append(np.zeros(hidden_dimensions))
 
-    # moving along the positions in the binary encoding
+    # Moving along the positions in the binary encoding
     for position in range(binary_dimensions):
 
-        # generate input and output
+        # Generate input and output
         X = np.array([[a[binary_dimensions - position - 1], b[binary_dimensions - position - 1]]])
         y = np.array([[c[binary_dimensions - position - 1]]]).T
 
-        # hidden layer 1
+        # Hidden layer 1
         # is passed through the sigmoid functions as a addition of the layers
         layer_1 = sigmoid(np.dot(X, synapse_0) + np.dot(layer_1_values[-1], synapse_h))
 
-        # output layer
-        # passes the output from our layers through the sigmoid function
+        # Output layer
+        # Passes the output from our layers through the sigmoid function
         # to give a prediction
         layer_2 = sigmoid(np.dot(layer_1, synapse_1))
 
-        # calculate the amount you are away from the true value (the error)
+        # Calculate the amount you are away from the true value (the error)
         layer_2_error = y - layer_2
         layer_2_deltas.append((layer_2_error) * sigmoid_output_to_derivative(layer_2))
         overallError += np.abs(layer_2_error[0])  # abs is the absolute value (square root of sum)
 
-        # decode estimate so we can print it out
+        # Decode estimate so we can print it out
         d[binary_dimensions - position - 1] = np.round(layer_2[0][0])
 
-        #store hidden layer so it can be used it in the next time-step
+        # Store hidden layer so it can be used it in the next time-step
         layer_1_values.append(copy.deepcopy(layer_1))
 
     future_layer_1_delta = np.zeros(hidden_dimensions)
@@ -137,12 +137,12 @@ for j in range(10000):
         layer_1 = layer_1_values[-position - 1]
         prev_layer_1 = layer_1_values[-position - 2]
 
-        # error at output layer
+        # Error at output layer
         layer_2_delta = layer_2_deltas[-position - 1]
         # error at hidden layer
         layer_1_delta = (future_layer_1_delta.dot(synapse_h.T) + layer_2_delta.dot(synapse_1.T)) * sigmoid_output_to_derivative(layer_1)
 
-        # This information is used to update the new weight update functions
+        # This information is used to update the weight functions
         synapse_1_update += np.atleast_2d(layer_1).T.dot(layer_2_delta)
         synapse_h_update += np.atleast_2d(prev_layer_1).T.dot(layer_1_delta)
         synapse_0_update += X.T.dot(layer_1_delta)
@@ -157,7 +157,7 @@ for j in range(10000):
     synapse_1_update *= 0
     synapse_h_update *= 0
 
-    # print out progress
+    # Print out of progress
     if(j % 1000 == 0):
         print("Model Error:" + str(overallError))
         print("Prediction Value:" + str(d))
